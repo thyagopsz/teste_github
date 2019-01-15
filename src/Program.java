d21354dfa78cf9a504f7c133b519264bd0982c82
Thiago Pereira <thiagoprasouza@gmail.com>
2019-01-15 12:41:06 -0200

    Mensagem PRINTAR

--- src/Program.java
+++ src/Program.java
@@ -3,5 +3,6 @@ public class Program {
     public static void main(String args []){
         System.out.println("pRINT");
         System.out.println("printar");
+        System.out.println("Boa tarde");
     }
 }
